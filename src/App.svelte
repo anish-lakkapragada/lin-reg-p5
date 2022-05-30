5360b34610b9fc0eb86a8e136b7a80f91d02d0e6
anish-lkakapragada <anish.lakkapragada@gmail.com>
2022-05-30 00:33:45 -0700

    ok i NEED to study spanish

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,11 @@
 	const xs = []; 
 	const ys = []; 
 
+	$: optimizer = tf.train.sgd(lr) || lr; 
+
 	let m, b; // the coefficients
 	let lr = 0.01; 
-	const optimizer = tf.train.sgd(lr);
+	let optimizer = tf.train.sgd(lr);
 	let epochs = 200; // hmm we could create like a slider for this 
 
 	const loss_fn = (pred, labels) => {
@@ -30,6 +32,10 @@
 		};	
 
 		p5.mousePressed = () => {
+
+			if ((p5.mouseX < 0 || p5.mouseY < 0) || (p5.mouseX > p5.width || p5.mouseY > p5.height)) {
+				return;
+			}
 
 			const x = p5.map(p5.mouseX, 0, p5.width, 0, 1); // convert to the graph coordinates
 			const y = p5.map(p5.mouseY, 0, p5.height, 1, 0); // go from 1 to 0 as higher up (lower value) means higher y-coordinate
@@ -75,15 +81,24 @@
 	<h1> Linear Regression Demo to Piss Off Ronak </h1>
 	<P5 {sketch} />
 
+	<br> 
 	<!-- add the sliders for the lr and epochs --> 	
 	<Form class="sliders"> 
 		<Slider label="learning rate" min="0" max="1" step="0.01" bind:value={lr}></Slider>
 		<br>
+		<br> 
 		<Slider label="epochs" min="1" max="200" step="1" bind:value={epochs}></Slider>
 	</Form>
+
+	<p style="font-size: 2em;"> Basically ronak doesn't like p5.js. i used papercss tho so i dont think he'll be too mad </p>
+	<footer> this is basically a small addition of the coding train challenge <a href="https://www.youtube.com/watch?v=dLp10CFIvxI"> in this video </a>  </footer>
 </div>
 
 <style> 
+	a {
+		text-decoration: none;
+	}
+
 	.center {
 		text-align: center;
 	}
